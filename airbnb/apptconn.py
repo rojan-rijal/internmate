3dd7b5113e4c961ea657e1f27f5d2f996f5d7807
janeenyamak <janeenyamak1@csu.fullerton.edu>
2020-04-14 22:19:44 -0700

    updated airbnb api

--- airbnb/apptconn.py
+++ airbnb/apptconn.py
@@ -8,12 +8,16 @@ def findApt(city,state):
     location = city+", "+state
     data = api.get_homes(location, offset = 1, items_per_grid = 1)
 
+    #print (data)
+
     for k in data['explore_tabs'][0]['sections'][0]['listings']:
         aptInfo['urls'] = k['listing']['picture_urls']
-        numOfBaths = k['listing']['bathroom_label']
-        
-        aptInfo['numbedroom']= k['listing']['bed_label']
-        aptInfo['rate']= k['pricing_quote']['rate_with_service_fee']['amount_formatted']
+        aptInfo['guests']=  k['listing']['guest_label'].split(' ')[0]
+        aptInfo['homeType']= k['listing']['bedroom_label'] 
+        aptInfo['numOfBaths'] = k['listing']['bathroom_label'].split(' ')[0]
+        numBedRooms = k['listing']['bed_label'].split(' ')[0]
+        aptInfo['numBedroom']= k['listing']['bed_label'].split(' ')[0]
+        aptInfo['rate']= k['pricing_quote']['rate_with_service_fee']['amount_formatted'].split('$')[1]
         apts.append(aptInfo)
 
     return apts
