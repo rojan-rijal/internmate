2c2df44fcdce25b8fa0bdf5acf5399b4f0f2e720
Rojan Rijal <32774729+rojan-rijal@users.noreply.github.com>
2020-04-15 20:35:53 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
-sudo apt-get install -y libmysqlclient-dev && apt-get install mysql-client
+sudo apt-get install docker.io
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker $(whoami)
+sudo apt-get install -y libmysqlclient-dev && sudo apt-get install mysql-client && sudo apt-get install python3-pip
 pip3 install -r requirements.txt
