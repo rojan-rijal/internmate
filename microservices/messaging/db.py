f97c1fb01a87525fb58a8830504c204ea319e143
Rojan <rojan@securifyinc.com>
2020-04-22 04:25:15 -0700

    Message feature done

--- microservices/messaging/db.py
+++ microservices/messaging/db.py
@@ -1,91 +1,41 @@
 from datetime import datetime
-
+import os, requests
 from bson import ObjectId
 from pymongo import MongoClient, DESCENDING
-from werkzeug.security import generate_password_hash
 
-from user import User
+client = MongoClient('mongodb://chat_admin:{db_password}@{db_ip}'.format(db_password=os.environ.get('CHAT_PASS'),
+                                                                        db_ip=os.environ.get('CHAT_IP')))
 
-client = MongoClient("mongodb+srv://test:test@chatapp-q7wkc.mongodb.net/test?retryWrites=true&w=majority")
-
-chat_db = client.get_database("ChatDB")
-users_collection = chat_db.get_collection("users")
-rooms_collection = chat_db.get_collection("rooms")
-room_members_collection = chat_db.get_collection("room_members")
-messages_collection = chat_db.get_collection("messages")
+chat_db = client.get_database("chatdb")
 
 
-def save_user(username, email, password):
-    password_hash = generate_password_hash(password)
-    users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
 
 
-def get_user(username):
-    user_data = users_collection.find_one({'_id': username})
-    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
+"""
+This is called when they add each other as friends
+"""
+def create_conversation(conversation_id, sender_name, sender_id, receiver):
+    collection = chat_db[conversation_id]
+    first_message = {"text":'{sender_name} added you as friend. Start a conversation :)'.format(sender_name=sender_name),
+                    'sender_id':sender_id,
+                    'created_at':datetime.now()}
+    collection.insert_one(first_message)
+
+def send_message(conversation_id, sender_id, text):
+    collection = chat_db[conversation_id]
+    post_message = {"text":text,
+                    "sender_id":sender_id,
+                    "created_at":datetime.now()}
+    collection.insert_one(post_message)
 
 
-def save_room(room_name, created_by):
-    room_id = rooms_collection.insert_one(
-        {'name': room_name, 'created_by': created_by, 'created_at': datetime.now()}).inserted_id
-    add_room_member(room_id, room_name, created_by, created_by, is_room_admin=True)
-    return room_id
-
-
-def update_room(room_id, room_name):
-    rooms_collection.update_one({'_id': ObjectId(room_id)}, {'$set': {'name': room_name}})
-    room_members_collection.update_many({'_id.room_id': ObjectId(room_id)}, {'$set': {'room_name': room_name}})
-
-
-def get_room(room_id):
-    return rooms_collection.find_one({'_id': ObjectId(room_id)})
-
-
-def add_room_member(room_id, room_name, username, added_by, is_room_admin=False):
-    room_members_collection.insert_one(
-        {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
-         'added_at': datetime.now(), 'is_room_admin': is_room_admin})
-
-
-def add_room_members(room_id, room_name, usernames, added_by):
-    room_members_collection.insert_many(
-        [{'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
-          'added_at': datetime.now(), 'is_room_admin': False} for username in usernames])
-
-
-def remove_room_members(room_id, usernames):
-    room_members_collection.delete_many(
-        {'_id': {'$in': [{'room_id': ObjectId(room_id), 'username': username} for username in usernames]}})
-
-
-def get_room_members(room_id):
-    return list(room_members_collection.find({'_id.room_id': ObjectId(room_id)}))
-
-
-def get_rooms_for_user(username):
-    return list(room_members_collection.find({'_id.username': username}))
-
-
-def is_room_member(room_id, username):
-    return room_members_collection.count_documents({'_id': {'room_id': ObjectId(room_id), 'username': username}})
-
-
-def is_room_admin(room_id, username):
-    return room_members_collection.count_documents(
-        {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'is_room_admin': True})
-
-
-def save_message(room_id, text, sender):
-    messages_collection.insert_one({'room_id': room_id, 'text': text, 'sender': sender, 'created_at': datetime.now()})
-
-
-MESSAGE_FETCH_LIMIT = 3
-
-
-def get_messages(room_id, page=0):
-    offset = page * MESSAGE_FETCH_LIMIT
+def get_messages(conversation_id, page=0):
+    offset = page * 0
+    collection = chat_db[conversation_id]
     messages = list(
-        messages_collection.find({'room_id': room_id}).sort('_id', DESCENDING).limit(MESSAGE_FETCH_LIMIT).skip(offset))
+        collection.find().sort('_id', DESCENDING).limit(0).skip(offset))
     for message in messages:
         message['created_at'] = message['created_at'].strftime("%d %b, %H:%M")
+        sender_info = requests.get('http://localhost:8000/api/user_exists/{id}'.format(id=message['sender_id'])).json()
+        message.update({'sender_name':sender_info['name'],'sender_image':sender_info['image']})
     return messages[::-1]
