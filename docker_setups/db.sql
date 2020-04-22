3035204deedfcdfabc5eb1c0656349628f3b20aa
Rojan <rojan@securifyinc.com>
2020-04-22 04:25:33 -0700

    Last update for the day

--- docker_setups/db.sql
+++ docker_setups/db.sql
@@ -4,8 +4,9 @@
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
-CREATE DATABASE internmate;
+create database internmate;
 use internmate;
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,6 +20,30 @@ use internmate;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `conversation`
+--
+
+DROP TABLE IF EXISTS `conversation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `conversation` (
+  `conversation_id` varchar(60) NOT NULL,
+  `last_updated` datetime NOT NULL,
+  PRIMARY KEY (`conversation_id`),
+  UNIQUE KEY `conversation_id` (`conversation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conversation`
+--
+
+LOCK TABLES `conversation` WRITE;
+/*!40000 ALTER TABLE `conversation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conversation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `friends`
 --
 
@@ -30,7 +55,9 @@ CREATE TABLE `friends` (
   `user1_id` int NOT NULL,
   `user2_id` int NOT NULL,
   `last_update_data` date NOT NULL,
-  UNIQUE KEY `friends_pair` (`user1_id`,`user2_id`)
+  `conversation_id` varchar(60),
+  UNIQUE KEY `friends_pair` (`user1_id`,`user2_id`),
+  UNIQUE KEY `conversation_id` (`conversation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -142,7 +169,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_id` (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -163,4 +190,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-15  2:51:54
+-- Dump completed on 2020-04-22  1:14:50
