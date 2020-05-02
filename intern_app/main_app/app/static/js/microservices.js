9987486ff78812d90788742cff93e8f4d0c3f29f
Rojan <rojan@securifyinc.com>
2020-05-01 23:23:21 -0700

    airbnb endpoints

--- intern_app/main_app/app/static/js/microservices.js
+++ intern_app/main_app/app/static/js/microservices.js
@@ -25,3 +25,46 @@ function recommendPlaces(city, state,offset) {
         }
     });
 };
+
+function airbnbs(city, state) {
+    var airbnb_url = "https://ab.internmate.tech/airbnb";
+    var airbnbs = [];
+    $.ajax({
+        url:airbnb_url,
+        dataType:'json',
+        type:'post',
+        contentType: 'application/json',
+        data: JSON.stringify({"city":city,"state":state}),
+        processData:false,
+        success: function(data, textStatus, jQxhr){
+            dataParsed = JSON.parse(JSON.stringify(data))
+            $.each(dataParsed, function(i, item) {
+                var parse = JSON.parse(JSON.stringify(item));
+                airbnbDatas = `
+                <div class="col col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+				<div class="shop-product-item">
+					<div class="product-thumb">
+						<img src="${parse.urls[0]}" alt="product">
+					</div>
+					<div class="product-content">
+						<div class="block-title">
+							<a href="#" class="product-category">COFFEE MUGS</a>
+							<a href="https://airbnb.com/rooms/${parse.id}" class="h5 title">${parse.name}</a>
+						</div>
+						<div class="block-price">
+							<div class="product-price">${parse.rate}</div>
+				
+							<a href="https://airbnb.com/${parse.id}" class="in-cart">
+								<svg class="olymp-shopping-bag-icon"><use xlink:href="/static/svg-icons/sprites/icons.svg##olymp-shopping-bag-icon"></use></svg>
+							</a>
+						</div>
+					</div>
+				</div>
+			    </div>
+                `
+                airbnbs.push(airbnbDatas);
+            });
+            $('#airbnbLists').html(airbnbs.join(""));
+        }
+    });
+};
